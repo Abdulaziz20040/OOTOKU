7a69a6f2e4a901b01fa515bfd7bb07df3491336d
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-07-08 23:32:19 +0500

    fsfd

--- src/app/user/settings/acaunt/page.jsx
+++ src/app/user/settings/acaunt/page.jsx
@@ -1,53 +1,61 @@
-"use client";
+// "use client";
+
+// import React from "react";
+// import { FcGoogle } from "react-icons/fc";
+
+// function Accaunt() {
+//   return (
+//     <div>
+//       <div className="w-[600px]">
+//         <h2 className="mb-4 text-lg font-semibold">
+//           Ijtimoiy tarmoq orqali kiring
+//         </h2>
+
+//         {/* Google login input */}
+//         <div className="mb-4">
+//           <label className="block mb-1 text-sm font-medium text-[#B1B1B1]">
+//             Google
+//           </label>
+//           <div className="relative">
+//             <span className="absolute inset-y-0 flex items-center left-3">
+//               <FcGoogle />
+//             </span>
+//             <input
+//               type="email"
+//               placeholder="example@gmail.com"
+//               className="w-full p-2 pl-10 bg-gray-100 border border-[#DEDEDE] outline-none rounded-lg"
+//               disabled
+//             />
+//           </div>
+//         </div>
+
+//         {/* Username input */}
+//         <div className="mb-4">
+//           <label className="block mb-1 text-sm font-medium text-[#B1B1B1]">
+//             Kirish
+//           </label>
+//           <input
+//             type="text"
+//             placeholder="John doe"
+//             className="w-full p-2 border border-[#DEDEDE] outline-none rounded-lg"
+//           />
+//         </div>
+
+//         {/* Save button */}
+//         <button className="w-full p-2 text-gray-500 bg-gray-300 rounded cursor-not-allowed">
+//           Saqlash
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Accaunt;
 
 import React from "react";
-import { FcGoogle } from "react-icons/fc";
 
-function Accaunt() {
-  return (
-    <div>
-      <div className="w-[600px]">
-        <h2 className="mb-4 text-lg font-semibold">
-          Ijtimoiy tarmoq orqali kiring
-        </h2>
-
-        {/* Google login input */}
-        <div className="mb-4">
-          <label className="block mb-1 text-sm font-medium text-[#B1B1B1]">
-            Google
-          </label>
-          <div className="relative">
-            <span className="absolute inset-y-0 flex items-center left-3">
-              <FcGoogle />
-            </span>
-            <input
-              type="email"
-              placeholder="example@gmail.com"
-              className="w-full p-2 pl-10 bg-gray-100 border border-[#DEDEDE] outline-none rounded-lg"
-              disabled
-            />
-          </div>
-        </div>
-
-        {/* Username input */}
-        <div className="mb-4">
-          <label className="block mb-1 text-sm font-medium text-[#B1B1B1]">
-            Kirish
-          </label>
-          <input
-            type="text"
-            placeholder="John doe"
-            className="w-full p-2 border border-[#DEDEDE] outline-none rounded-lg"
-          />
-        </div>
-
-        {/* Save button */}
-        <button className="w-full p-2 text-gray-500 bg-gray-300 rounded cursor-not-allowed">
-          Saqlash
-        </button>
-      </div>
-    </div>
-  );
+function page() {
+  return <div>page</div>;
 }
 
-export default Accaunt;
+export default page;
